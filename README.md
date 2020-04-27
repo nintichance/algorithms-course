36ef1643f46a368dd6088b9b45d064ea054f8c4f
Ninti Chance <ninhursag_chance@homedepot.com>
2020-04-26 22:53:01 -0400

    Update isFull method to check if the current site connects to a site on the 0th row. Add logic to determine if the system percolates.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Percolation Problem Solution
+**By Ninti Chance**
+
+This repository contains my solution for determining when a system percolates. 
+
+It solves the problem defined in the `Algorithms: Part 1` Course by Kevin Wayne and Robert Sedgewick on Percolation.
+If you have any questions regarding my solution, feel free to reach out to me.
+ 
--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -26,7 +26,6 @@ public class Percolation {
         grid[i][j] = -1;
       }
     }
-    System.out.println("ninti" + grid[0][0]);
   }
 
   // opens the site (row, col) if it is not open already
@@ -50,6 +49,69 @@ public class Percolation {
       }catch(Exception e){
         System.out.println(e);
       }
+    }
+  }
+
+  // is the site (row, col) open?
+  public boolean isOpen(int row, int col){
+    validate(row, col);
+    if (grid[row][col] != -1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  // is the site (row, col) full?
+  public boolean isFull(int row, int col){
+    int connection = 0;
+     for(int i=0; i<sizeOfGrid; i++){
+      if(grid[currentRow][currentCol] == grid[0][i]){
+        connection+=1;
+      }
+     }
+     if(connection>0){
+       return true;
+     }else{
+       return false;
+     }
+  }
+
+  // returns the number of open sites
+  public int numberOfOpenSites(){
+    return numOfOpenSites;
+  }
+
+
+  public boolean percolates(){
+    if(currentRow == sizeOfGrid-1){
+      System.out.println("GETS HERE");
+      boolean isPercolated = isFull(currentRow, currentCol);
+      return isPercolated;
+    }else{
+      return false;
+    }
+  }
+
+  //private methods
+  private int mapCellValueToUnionFindModel(int row,int col){
+    return sizeOfGrid*row+col;
+  }
+
+  private void validate(int row, int col){
+    if (row < 0 || col < 0){
+      System.out.println("Row in error:" + row);
+      System.out.println("Col in error:" + row);
+      throw new IllegalArgumentException("row or column must be greater than 0");
+    }
+  }
+
+  private void connectOpenAdjacents(int row, int col, int p){
+    validate(row, col);
+    if(isOpen(row, col)){
+      int q = mapCellValueToUnionFindModel(row, col);
+      weightedQuickUnionUF.union(p, q);
+      grid[row][col] = weightedQuickUnionUF.find(q);
     }
   }
 
@@ -90,91 +152,59 @@ public class Percolation {
     }
   }
 
-  private void connectOpenAdjacents(int row, int col, int p){
-    validate(row, col);
-    currentRow = row;
-    currentCol = col;
-    if(isOpen(row, col) || isFull(row, col)){
-      int q = mapCellValueToUnionFindModel(row, col);
-      weightedQuickUnionUF.union(p, q);
-      grid[row][col] = weightedQuickUnionUF.find(q);
-    }
-  }
-
-  // is the site (row, col) open?
-  public boolean isOpen(int row, int col){
-    validate(row, col);
-    if (grid[row][col] == mapCellValueToUnionFindModel(row, col)){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  // is the site (row, col) full?
-  public boolean isFull(int row, int col){
-    if (grid[row][col] != mapCellValueToUnionFindModel(row, col) && grid[row][col] != -1){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  // returns the number of open sites
-  public int numberOfOpenSites(){
-    return numOfOpenSites;
-  }
-
-
-  public boolean percolates(){
-    System.out.println("PERCOLATES?" + currentCol);
-    System.out.println("PERCOLATES?" + currentRow);
-    if(numOfOpenSites < (sizeOfGrid*sizeOfGrid)/2){
-      return false;
-    }
-    return true;
-    //if a site on the top row (0) connects to a site on the bottom row (sizeOfGrid-1), the system percolates
-//    grid[0][N] && grid[sizeOfGrid][N]
-    //pick full site in the grid
-    //see if it is connected to something connected to something on the top
-
-  }
-
-  private int mapCellValueToUnionFindModel(int row,int col){
-    return sizeOfGrid*row+col;
-  }
-
-  private void validate(int row, int col){
-    if (row < 0 || col < 0){
-      System.out.println("Row in error:" + row);
-      System.out.println("Col in error:" + row);
-      throw new IllegalArgumentException("row or column must be greater than 0");
-    }
-  }
-  // test client (optional)
-  public static void main(String[] args){
-    Percolation perc = new Percolation(3);
-    perc.open(0,0);
-    perc.open(2,2);
-    perc.open(1,0);
-    perc.open(2,1);
-    perc.open(0,2);
-    perc.open(2,0);
-    perc.open(0,1);
-    perc.open(1,2);
-    perc.open(1,1);
-
-    System.out.println("0---:" + grid[0][0]);
-    System.out.println("1---:" + grid[0][1]);
-    System.out.println("2---:" + grid[0][2]);
-    System.out.println("3---:" + grid[1][0]);
-    System.out.println("4---:" + grid[1][1]);
-    System.out.println("5---:" + grid[1][2]);
-    System.out.println("6---:" + grid[2][0]);
-    System.out.println("7---:" + grid[2][1]);
-    System.out.println("8---:" + grid[2][2]);
-    System.out.println("num---:" + numOfOpenSites);
-  }
+//  private static void printStatements(){
+//    System.out.println("0---[0][0]:" + grid[0][0]);
+//    System.out.println("1---[0][1]:" + grid[0][1]);
+//    System.out.println("2---[0][2]:" + grid[0][2]);
+//    System.out.println("3---[1][0]:" + grid[1][0]);
+//    System.out.println("4---[1][1]:" + grid[1][1]);
+//    System.out.println("5---[1][2]:" + grid[1][2]);
+//    System.out.println("6---[2][0]:" + grid[2][0]);
+//    System.out.println("7---[2][1]:" + grid[2][1]);
+//    System.out.println("8---grid[2][2]:" + grid[2][2]);
+//  }
+//  // test client (optional)
+//  public static void main(String[] args){
+//    Percolation perc = new Percolation(3);
+//    perc.open(0,0);
+//
+//    System.out.println("ONE");
+//    System.out.println(perc.percolates());
+//    perc.open(2,2);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("TWO");
+//    perc.open(1,0);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("THREE");
+//    perc.open(2,1);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("FOUR");
+//    perc.open(0,2);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("FIVE");
+//    perc.open(2,0);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("SIX");
+//    perc.open(0,1);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("SEVEN");
+//    perc.open(1,2);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("EIGHT");
+//    perc.open(1,1);
+//    printStatements();
+//    System.out.println(perc.percolates());
+//    System.out.println("NINE");
+//    printStatements();
+//
+//  }
 
 }
 
